df0c82987ede6fc5a4c5dfdfdab6539f78714e4c
ROHIT A S NAIR <rohitasnair111@gmail.com>
2019-09-07 21:31:24 +0530

    Fixed pdf page missing issues and adjusted text area

--- text/add text.py
+++ text/add text.py
@@ -1,26 +1,40 @@
 from pyPdf import PdfFileWriter, PdfFileReader
 import StringIO
+import os
+import PyPDF2
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
-a = int(input("x-axis: "))
-b = int(input("y-axis: "))
-#font
+#import PyPDF2 
+
+def PDFmerge(pdfs, output): 
+	# creating pdf file merger object 
+	pdfMerger = PyPDF2.PdfFileMerger() 
+	
+	# appending pdfs one by one 
+	for pdf in pdfs: 
+		with open(pdf, 'rb') as f: 
+			pdfMerger.append(f) 
+		
+	# writing combined pdf to output pdf file 
+	with open(output, 'wb') as f: 
+		pdfMerger.write(f) 
+
 pdfmetrics.registerFont(TTFont('Round', 'RoundhandBT.ttf'))
 
 packet = StringIO.StringIO()
 # create a new PDF with Reportlab
 can = canvas.Canvas(packet, pagesize=letter)
 can.setFont('Round', 32)
-can.drawString(a, b, "Hello world")
+can.drawString(200, 510, "Hello world")
 can.save()
 
 #move to the beginning of the StringIO buffer
 packet.seek(0)
 new_pdf = PdfFileReader(packet)
 # read your existing PDF
-existing_pdf = PdfFileReader(file("2c.pdf", "rb"))
+existing_pdf = PdfFileReader(file("1.pdf", "rb"))
 output = PdfFileWriter()
 # add the "watermark" (which is the new pdf) on the existing page
 page = existing_pdf.getPage(0)
@@ -30,3 +44,8 @@ output.addPage(page)
 outputStream = file("destination.pdf", "wb")
 output.write(outputStream)
 outputStream.close()
+
+pdfs = ['destination.pdf', '2.pdf'] 
+output = 'final.pdf'
+PDFmerge(pdfs = pdfs, output = output) 
+os.remove("destination.pdf")
